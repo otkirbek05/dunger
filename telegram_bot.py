1e88447aa0431ec4642a2e2422021376162ca533
otkirbek05 <mamurovotkirbek0719@gmail.com>
2023-11-21 11:45:06 +0500

    adding register site

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,13 +1,9 @@
-from telegram import Update
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, Updater, CallbackQueryHandler
 import logging
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import (
+    ApplicationBuilder, CommandHandler, ContextTypes, Updater, CallbackQueryHandler, filters)
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(
-        f'Hello welcome to our bot {update.effective_user.first_name} first you should register'
-        
-        )
+
 
 
 async def menu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -66,23 +62,58 @@ logger = logging.getLogger(__name__)
 
 FIRST_NAME, LAST_NAME, LOCATION, PHONE_NUMBER = range(4)
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(
+        f"Hello welcome to our bot {update.effective_user.first_name}! \n"
+        "First you should register"
+        
+        )
+
+    return FIRST_NAME
+
+async def first_name(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores the selected gender and asks for a photo."""
+    user = update.message.from_user
+    await update.message.reply_text(
+        f'Enter your first name'
+        
+        )
+    return LAST_NAME
 
 
+async def last_name(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores the photo and asks for a location."""
+    user = update.message.from_user
+    photo_file = await update.message.photo[-1].get_file()
+    await photo_file.download_to_drive("user_photo.jpg")
+    logger.info("Photo of %s: %s", user.first_name, "user_photo.jpg")
+    await update.message.reply_text(
+        "Gorgeous! Now, send me your location please, or send /skip if you don't want to."
+    )
+
+    return LOCATION
+
+async def location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores the location and asks for some info about the user."""
+    user = update.message.from_user
+    user_location = update.message.location
+    logger.info(
+        "Location of %s: %f / %f", user.first_name, user_location.latitude, user_location.longitude
+    )
+    await update.message.reply_text(
+        "Maybe I can visit you sometime! At last, tell me something about yourself."
+    )
 
 
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Cancels and ends the conversation."""
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    await update.message.reply_text(
+        "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
+    )
 
-
-
-
-
-
-
-
-
-
-
-
-
+    return ConversationHandler.END
 
 
 def main() -> None:
@@ -94,7 +125,7 @@ def main() -> None:
 
 
     app.add_handler(CallbackQueryHandler(button))
-    app.add_handler(CommandHandler("hello", hello))
+    app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler('menu', menu))
     app.run_polling(allowed_updates=Update.ALL_TYPES)
 
